3083eba769c0cf938820495ac1a089441403d96f
Matias Leitner <leitnermatias22@gmail.com>
2024-11-12 18:48:16 -0300

    Start errors implementation

--- src/main.ts
+++ src/main.ts
@@ -28,30 +28,8 @@ window.onload = async () => {
 
   try {
     await startProgram(new Terminal())
-    await startProgram(new Terminal())
   } catch (error) {
     console.error(error)
   }
-
-  // try {
-  //   const data = [
-  //     ["id", "name", "email"],
-  //     [1, "John Doe", "johndoe@example.com"],
-  //     [2, "Jane Smith", "janesmith@example.com"],
-  //     [3, "Chris Johnson", "chrisj@example.com"]
-  //   ];
-
-  //   const csvContent = data.map(e => e.join(",")).join("\n");
-  //   const blob = new Blob([csvContent], { type: "text/csv" });
-
-  //   const f1 = new SystemFile({name: 'Test in init', path: '/tmp', contents: new File([blob], 'test in init.csv', {type: 'text/csv'})})
-  //   await f1.save()
-
-  //   const f1new = await FileSystem.Get("Test in init")
-
-  //   console.log(f1new)
-  // } catch (error) {
-  //   console.error("Error saving file ", error)
-  // }
   requestAnimationFrame(loop)
 }
